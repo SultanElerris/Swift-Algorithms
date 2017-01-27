d0c4db8c0d038c1e26ec6a4de43ead459aa5a974
Sultan <srs389@psu.edu>
2017-01-26 20:26:49 -0500

    initial commit

---
+++ FuzzBuzz.swift
@@ -0,0 +1,14 @@
+
+import UIKit
+
+var numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+
+for number in numbers {
+    if (number % 3) == 0 && (number % 5) == 0 {
+        print ("\(number)FuzzBuzz")
+    } else if number % 3 == 0 {
+        print ("\(number)Fuzz")
+    } else if number % 5 == 0 {
+        print ("\(number)Buzz")
+    }
+}
